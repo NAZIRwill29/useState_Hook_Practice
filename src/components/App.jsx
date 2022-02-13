cbef902d395d89000f91b96c121080acb50bd161
NAZIRwill29 <59091291+NAZIRwill29@users.noreply.github.com>
2022-02-13 14:30:54 +0800

    update time like clock execute function for every second

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  //set current time
+  let time = new Date().toLocaleTimeString();
+  //console.log(time);
+
+  let [currentTime, setTime] = useState(time);
+
+  //get current time when btn clicked
+  function getCurrentTime() {
+    setTime((currentTime = new Date().toLocaleTimeString()));
+  }
+
+  //execute for every second
+  setInterval(getCurrentTime, 1000);
+
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{currentTime}</h1>
+      <button onClick={getCurrentTime}>Get Time</button>
     </div>
   );
 }
